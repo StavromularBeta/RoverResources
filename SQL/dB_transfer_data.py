6f89bc3db77591ee849bdae9c11d78dd9351053b
Peter Levett <peterlevett@gmail.com>
2021-11-05 12:59:36 -0700

    wrestling with data transfer.

--- SQL/dB_transfer_data.py
+++ SQL/dB_transfer_data.py
@@ -8,24 +8,23 @@ class DataTransfer(Connector):
         super(self.__class__, self).__init__()
 
     def insert_db_one_table_one_into_db_two_table_two(self,
-                                                      database_2_name,
                                                       table_name):
-        query = "ATTACH DATABASE " + database_2_name + " AS old_db; "
+        query = "ATTACH DATABASE " + self.old_database_target + " AS old_db; "
         query += "INSERT INTO " + table_name + " SELECT * FROM old_db." + table_name + "; "
         query += "DETACH old_db;"
         return self.db_connector(query)
 
 
 data_transfer = DataTransfer()
-data_transfer.insert_db_one_table_one_into_db_two_table_two("RoverResourcesDatabase2", "categories")
-data_transfer.insert_db_one_table_one_into_db_two_table_two("RoverResourcesDatabase2", "sub_categories")
-data_transfer.insert_db_one_table_one_into_db_two_table_two("RoverResourcesDatabase2", "vendors")
-data_transfer.insert_db_one_table_one_into_db_two_table_two("RoverResourcesDatabase2", "products")
-data_transfer.insert_db_one_table_one_into_db_two_table_two("RoverResourcesDatabase2", "priceTracking")
-data_transfer.insert_db_one_table_one_into_db_two_table_two("RoverResourcesDatabase2", "credentials")
-data_transfer.insert_db_one_table_one_into_db_two_table_two("RoverResourcesDatabase2", "users")
-data_transfer.insert_db_one_table_one_into_db_two_table_two("RoverResourcesDatabase2", "requests")
-data_transfer.insert_db_one_table_one_into_db_two_table_two("RoverResourcesDatabase2", "orders")
-data_transfer.insert_db_one_table_one_into_db_two_table_two("RoverResourcesDatabase2", "received")
-data_transfer.insert_db_one_table_one_into_db_two_table_two("RoverResourcesDatabase2", "inventory")
+data_transfer.insert_db_one_table_one_into_db_two_table_two("categories")
+data_transfer.insert_db_one_table_one_into_db_two_table_two("sub_categories")
+data_transfer.insert_db_one_table_one_into_db_two_table_two("vendors")
+data_transfer.insert_db_one_table_one_into_db_two_table_two("products")
+data_transfer.insert_db_one_table_one_into_db_two_table_two("priceTracking")
+data_transfer.insert_db_one_table_one_into_db_two_table_two("credentials")
+data_transfer.insert_db_one_table_one_into_db_two_table_two("users")
+data_transfer.insert_db_one_table_one_into_db_two_table_two("requests")
+data_transfer.insert_db_one_table_one_into_db_two_table_two("orders")
+data_transfer.insert_db_one_table_one_into_db_two_table_two("received")
+data_transfer.insert_db_one_table_one_into_db_two_table_two("inventory")
 
